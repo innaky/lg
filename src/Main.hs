abfe3b40ae56cd35021264fce006d804f85d2db1
Innaky <innaky@protonmail.com>
2020-02-21 23:54:40 -0400

    Getting a file size in bytes.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,16 @@
 module Main where
 
+import System.Posix.Files (getFileStatus, fileSize)
+import System.Posix.Types (FileOffset)
+import System.Environment (getArgs)
+
+getFileSize :: FilePath -> IO (FileOfset)
+getFileSize filePath = do
+  status <- getFileStatus filePath
+  return (fileSize status)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  (filepath:_) <- getArgs
+  size <- getFileSize filepath
+  print size
